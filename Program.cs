83c1bc7f5073bdbbcd507f4db6bf92d3dc944ea9
Maksat Kuzyev <10titan40@gmail.com>
2022-12-01 23:24:20 +0300

    Hard Homework

---
+++ Program.cs
@@ -0,0 +1,148 @@
+﻿// Задача 1
+
+// Console.WriteLine("Введите число 1 : ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число 2 : ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a>b)
+//     Console.Write("Число 1 больше чиcла 2");
+// else if (a<b)
+//     Console.Write("Число 1 меньше числа 2");
+
+
+// Задача 2
+
+// Console.WriteLine("Введите число 1 : ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число 2 : ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число 3 : ");
+// int c = Convert.ToInt32(Console.ReadLine());
+
+// if (a > b && a > c)
+//     Console.Write("Max = " + a);
+// if (b > a && b > c)
+//     Console.Write("Max = " + b);
+// if (c > a && c > b)
+//     Console.Write("Max = " + c);
+
+
+// Задача 3
+
+// Console.WriteLine("Введите число : ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// if (a%2==0)
+//     Console.WriteLine("Число является четным.");
+// else
+//     Console.WriteLine("Число не является четным");
+
+
+// Задача 4
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int x = 2; x < n; x++)       
+if (x%2==0)            
+Console.WriteLine(x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Console.WriteLine("How old are you?");
+// int test = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("After 6 year you will be a "+(test+6)+" years old");
+
+// Console.Write("Enter your number: ");
+// int numb = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Square of number {numb} is {numb*numb}");
+
+// Console.WriteLine("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if (b*b == a)
+// {
+//     Console.WriteLine($"Число {a} является квадратом числа {b}");
+// }
+// else
+// {
+//     Console.WriteLine("Не является");
+// }
+
+
+// Console.Write("Enter number of day: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if(num == 1)
+//     Console.WriteLine("Понедельник");
+// else if(num == 2)
+//     Console.WriteLine("Вторник");
+// else if(num == 3)
+//     Console.WriteLine("Среда");
+// else if(num == 4)
+//     Console.WriteLine("Четверг");
+// else if(num == 5)
+//     Console.WriteLine("Пятница");
+// else if(num == 6)
+//     Console.WriteLine("Суббота");
+// else if(num == 7)
+//     Console.WriteLine("Воскресенье");
+
+// else
+//     Console.WriteLine("Error!");
+
+//Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+// Console.WriteLine("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int t = n*(-1);
+
+// while (t<=n)
+// {
+//     Console.Write(" " + t);
+
+//     t+=1;
+// }
+
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+// Console.WriteLine("Введите трехзначное число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num>99 && num<1000)
+// {
+//     Console.WriteLine(num%10);
+// }
+// else
+// {
+//         Console.WriteLine("Неверное число!");
+// }
+
